c423011fae5a646fb89ff1424388ec1ee7a5a1a1
fumiyama6256 <au.ver.c.e.2@gmail.com>
2022-09-12 23:13:00 +0900

    APIキーを追加

--- src/conf/utils/skyway.ts
+++ src/conf/utils/skyway.ts
@@ -3,7 +3,7 @@ import Peer, { SfuRoom } from "skyway-js";
 export const initPeer = (forceTurn: boolean): Promise<Peer> => {
   return new Promise((resolve, reject) => {
     const peer = new Peer({
-      key: "",
+      key: "787acc6d-f4a8-4a53-9b6a-586ca797cc16",
       debug: 2,
       config: {
         iceTransportPolicy: forceTurn ? "relay" : "all",
